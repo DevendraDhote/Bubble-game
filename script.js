a711334efce81334d2baf8675a835ab869981f38
Devendradhote <83503966+Devendradhote@users.noreply.github.com>
2021-09-03 23:52:59 +0530

    Drag drop

---
+++ script.js
@@ -0,0 +1,61 @@
+var bottom = document.querySelector('#bottom');
+var timerelem = document.querySelector('#timer');
+var click = document.querySelector('#click');
+var scoreelem = document.querySelector('#score');
+var modal = document.querySelector('#modal');
+
+var score = 0;
+var randomnum;
+var timer = 10;
+var temp = ``;
+
+function body(){
+    for(i = 0; i <=97; i++){
+        var guessnum = Math.floor(Math.random()*10);
+        temp += `<div id="bubble">${guessnum}</div>`
+    }
+    
+    bottom.innerHTML = temp;
+}
+
+function TimerFunc(){
+    setInterval(() => {
+        if(timer === -1){
+            clearInterval()
+            bottom.innerHTML = "";
+            modal.style.display = 'initial';
+        }
+        else{
+            timerelem.textContent = timer--;
+        }
+    }, 1000);
+}
+
+function Clicked(){
+    randomnum = Math.floor(Math.random()*10)
+    click.textContent = randomnum;
+}
+
+// function IncreaseScore(){
+//     score = score + 10;
+//     scoreelem.textContent = score;
+// }
+
+bottom.addEventListener('click', function(dets){
+    var Clickedbubble = dets.target.textContent;
+    if(randomnum == Clickedbubble ){
+        score = score + 10;
+        scoreelem.textContent = score;
+        Clicked();
+        bottom.innerHTML = "";
+        temp = ``;
+        body();
+        
+    }else{
+        bottom.innerHTML = ""; 
+    }    
+});
+
+Clicked();
+// TimerFunc();
+body();
